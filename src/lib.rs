4ca33c110451cab121d8e060e85eae23e2d0f071
Katsu Kawakami <kkawa1570@gmail.com>
2015-03-23 22:20:20 -0400

    Check if stdin is from a tty

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
-#[test]
-fn it_works() {
+#![feature(libc)]
+extern crate libc;
+
+pub fn readline() -> Option<String> {
+    // Buffer to hold readline input
+    let buffer = String::new();
+
+    let isatty = unsafe { libc::isatty(libc::STDIN_FILENO as i32) } != 0; 
+    if isatty  {
+        println!("stdin is a tty");
+    } else {
+        println!("stdin is not a tty");
+    }
+
+    buffer
 }

30985523b35df85d3454060e2167b51a7f402474
Katsu Kawakami <kkawa1570@gmail.com>
2015-03-23 22:47:38 -0400

    Split out tty check to a separate function

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
 #![feature(libc)]
 extern crate libc;
 
-pub fn readline() -> Option<String> {
-    // Buffer to hold readline input
-    let buffer = String::new();
 
-    let isatty = unsafe { libc::isatty(libc::STDIN_FILENO as i32) } != 0; 
-    if isatty  {
-        println!("stdin is a tty");
-    } else {
-        println!("stdin is not a tty");
-    }
 
-    buffer
+fn isatty() -> bool {
+    let isatty = unsafe { libc::isatty(libc::STDIN_FILENO as i32) } != 0;
+    isatty
 }
+
+pub fn readline() -> Option<String> {
+    if isatty() {
+        Some(buffer)
+    } else {
+        None
+    }
+}
+
+
+

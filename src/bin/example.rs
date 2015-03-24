d1a1beb59ee3cacff12348771f492d588e5f5386
Katsu Kawakami <kkawa1570@gmail.com>
2015-03-23 22:45:01 -0400

    Update example to match against Option<String> returned by readline

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -1,5 +1,9 @@
 extern crate rustyline;
 
 fn main() {
-    rustyline::readline();
+    let readline = rustyline::readline();
+    match readline {
+        Some(line) => println!("Line: {:?}",line),
+        None       => println!("No input"),
+    }
 }

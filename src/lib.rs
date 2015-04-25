319eb0244c03134cd0046e602497b3c51ca9bf43
Main <kkawa1570@gmail.com>
2015-04-24 23:02:07 -0400

    Completed enable_raw_mode function

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,15 @@ extern crate libc;
 extern crate nix;
 
 use std::io;
-use std::io::{Write, Read};
-use std::env;
-use nix::errno;
+use std::io::{Write, Read, Error, ErrorKind};
+use nix::errno::Errno;
+use nix::Error::Sys;
+use nix::sys::termios;
+use nix::sys::termios::{BRKINT, ICRNL, INPCK, ISTRIP, IXON, OPOST, CS8, ECHO, ICANON, IEXTEN, ISIG, VMIN, VTIME};
 
 static MAX_LINE: i32 = 4096;
 static UNSUPPORTED_TERM: [&'static str; 3] = ["dumb","cons25","emacs"];
+static RAW_MODE_ENABLED: bool = false;
 
 
 fn is_a_tty() -> bool {
@@ -16,7 +19,7 @@ fn is_a_tty() -> bool {
 }
 
 fn is_unsupported_term() -> bool {
-    let term = env::var("TERM").ok().unwrap();
+    let term = std::env::var("TERM").ok().unwrap();
 
     let mut unsupported = false;
     for iter in &UNSUPPORTED_TERM {
@@ -25,8 +28,19 @@ fn is_unsupported_term() -> bool {
     unsupported
 }
 
-fn enable_raw_mode(fd: i64) -> Result<(), errno::Errno> {
-    Ok(())
+fn enable_raw_mode() -> Result<(), nix::Error> {
+    if !is_a_tty() {
+        Err(Sys(Errno::ENOTTY)) 
+    } else {
+        let mut raw = try!(termios::tcgetattr(libc::STDIN_FILENO));
+        raw.c_iflag = raw.c_iflag   & !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+        raw.c_oflag = raw.c_oflag   & !(OPOST);
+        raw.c_cflag = raw.c_cflag   | (CS8);
+        raw.c_lflag = raw.c_lflag   & !(ECHO | ICANON | IEXTEN | ISIG);
+        raw.c_cc[VMIN] = 1;
+        raw.c_cc[VTIME] = 0;
+        Ok(())
+    }
 }
 
 fn readline_raw() -> Result<String, io::Error> {
@@ -41,6 +55,13 @@ fn readline_raw() -> Result<String, io::Error> {
 
         Ok(String::from_utf8(buffer).unwrap())
     } else {
+        match enable_raw_mode() {
+            Err(Sys(Errno::ENOTTY)) => return Err(Error::new(ErrorKind::Other, "Not a TTY")),
+            Err(Sys(Errno::EBADF))  => return Err(Error::new(ErrorKind::Other, "Not a file descriptor")),
+            Err(e)                  => return Err(Error::new(ErrorKind::Other, "Unknown Error")),
+            Ok(_)                   => ()
+        }
+
         let mut line = String::new();
         match io::stdin().read_line(&mut line) {
             Ok(_) => Ok(line),

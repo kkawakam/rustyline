ad255615ab0210a77473c719383babaed077db2a
Main <kkawa1570@gmail.com>
2015-04-28 00:51:52 -0400

    * Functions to enable and disable raw mode now work * You can now read 1 character at a time in raw mode

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,6 @@ use nix::sys::termios::{BRKINT, ICRNL, INPCK, ISTRIP, IXON, OPOST, CS8, ECHO, IC
 
 static MAX_LINE: i32 = 4096;
 static UNSUPPORTED_TERM: [&'static str; 3] = ["dumb","cons25","emacs"];
-static RAW_MODE_ENABLED: bool = false;
-
 
 fn is_a_tty() -> bool {
     let isatty = unsafe { libc::isatty(libc::STDIN_FILENO as i32) } != 0;
@@ -20,27 +18,33 @@ fn is_a_tty() -> bool {
 
 fn is_unsupported_term() -> bool {
     let term = std::env::var("TERM").ok().unwrap();
-
     let mut unsupported = false;
     for iter in &UNSUPPORTED_TERM {
-        unsupported = (term == *iter)
+        unsupported = term == *iter
     }
     unsupported
 }
 
-fn enable_raw_mode() -> Result<(), nix::Error> {
+fn enable_raw_mode() -> Result<termios::Termios, nix::Error> {
     if !is_a_tty() {
         Err(Sys(Errno::ENOTTY)) 
     } else {
-        let mut raw = try!(termios::tcgetattr(libc::STDIN_FILENO));
+        let original_term = try!(termios::tcgetattr(libc::STDIN_FILENO));
+        let mut raw = original_term;
         raw.c_iflag = raw.c_iflag   & !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
         raw.c_oflag = raw.c_oflag   & !(OPOST);
         raw.c_cflag = raw.c_cflag   | (CS8);
         raw.c_lflag = raw.c_lflag   & !(ECHO | ICANON | IEXTEN | ISIG);
         raw.c_cc[VMIN] = 1;
         raw.c_cc[VTIME] = 0;
-        Ok(())
+        try!(termios::tcsetattr(libc::STDIN_FILENO, termios::TCSAFLUSH, &raw));
+        Ok(original_term)
     }
+}
+
+fn disable_raw_mode(original_termios: termios::Termios) -> Result<(), nix::Error> {
+    try!(termios::tcsetattr(libc::STDIN_FILENO, termios::TCSAFLUSH, &original_termios));
+    Ok(())
 }
 
 fn readline_raw() -> Result<String, io::Error> {
@@ -48,19 +52,24 @@ fn readline_raw() -> Result<String, io::Error> {
     let mut input: [u8; 1] = [0];
 
     if is_a_tty() {
-        let numread = io::stdin().take(1).read(&mut input).unwrap();
-
-        println!("Read #{:?} bytes with a value of{:?}",numread,input[0]);
+        let original_termios = match enable_raw_mode() {
+            Err(Sys(Errno::ENOTTY)) => return Err(Error::new(ErrorKind::Other, "Not a TTY")),
+            Err(Sys(Errno::EBADF))  => Err(Error::new(ErrorKind::Other, "Not a file descriptor")),
+            Err(..)                 => Err(Error::new(ErrorKind::Other, "Unknown Error")),
+            Ok(term)                => term
+        };
+        
+        let numread = io::stdin().read(&mut input).unwrap();
         buffer.push(input[0]);
+        println!("Read #{:?} bytes with a value of{:?}",numread,input[0]);
 
+
+        match disable_raw_mode(original_termios) {
+            Err(..) => return Err(Error::new(ErrorKind::Other, "Failed to revert to original termios")),
+            Ok(..)  => ()
+        }
         Ok(String::from_utf8(buffer).unwrap())
     } else {
-        match enable_raw_mode() {
-            Err(Sys(Errno::ENOTTY)) => return Err(Error::new(ErrorKind::Other, "Not a TTY")),
-            Err(Sys(Errno::EBADF))  => return Err(Error::new(ErrorKind::Other, "Not a file descriptor")),
-            Err(e)                  => return Err(Error::new(ErrorKind::Other, "Unknown Error")),
-            Ok(_)                   => ()
-        }
 
         let mut line = String::new();
         match io::stdin().read_line(&mut line) {
@@ -73,8 +82,8 @@ fn readline_raw() -> Result<String, io::Error> {
 pub fn readline(prompt: &'static str) -> Result<String, io::Error> {
     // Write prompt and flush it to stdout
     let mut stdout = io::stdout();
-    stdout.write(prompt.as_bytes());
-    stdout.flush();
+    try!(stdout.write(prompt.as_bytes()));
+    try!(stdout.flush());
 
     if is_unsupported_term() {
         let mut line = String::new();

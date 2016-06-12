e20f91c5f7028bfd3bd9050192bbd5536f7b21fd
Katsu Kawakami <kkawa1570@gmail.com>
2016-06-11 23:38:15 -0400

    Created Terminal Trait for different platform TTYs
    Terminal trait is defined in tty/common.rs where it requires
    implementation of enable_raw_mode and disable_raw_mode.  readline_edit
    will now move around a mutable reference to a Terminal instead of the
    platform specific struct.

--- src/tty/common.rs
+++ src/tty/common.rs
@@ -1,4 +1,16 @@
+//! This module implements and describes common TTY methods & traits
 extern crate libc;
+use super::Result;
+
+/// Trait that should be for each TTY/Terminal on various platforms
+/// (e.g. unix & windows)
+pub trait Terminal {
+    /// Enable RAW mode for the terminal
+    fn enable_raw_mode(&mut self) -> Result<()>;
+
+    /// Disable RAW mode for the terminal
+    fn disable_raw_mode(&self) -> Result<()>;
+}
 
 /// Check to see if `fd` is a TTY
 pub fn is_a_tty(fd: libc::c_int) -> bool {

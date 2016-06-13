328c0e2c9a66a882774f755d4754f7ef27c4c27b
kkawakam <kkawa1570@gmail.com>
2016-06-13 01:50:56 -0400

    Add conditional compilation for unix errors

--- src/error.rs
+++ src/error.rs
@@ -12,24 +12,26 @@ use char_iter;
 pub enum ReadlineError {
     /// I/O Error
     Io(io::Error),
-    /// Error from syscall
-    Errno(nix::Error),
     /// Chars Error
     Char(char_iter::CharsError),
     /// EOF (Ctrl-d)
     Eof,
     /// Ctrl-C
     Interrupted,
+    /// Unix Error from syscall
+    #[cfg(unix)]
+    Errno(nix::Error),
 }
 
 impl fmt::Display for ReadlineError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             ReadlineError::Io(ref err) => err.fmt(f),
-            ReadlineError::Errno(ref err) => write!(f, "Errno: {}", err.errno().desc()),
             ReadlineError::Char(ref err) => err.fmt(f),
             ReadlineError::Eof => write!(f, "EOF"),
             ReadlineError::Interrupted => write!(f, "Interrupted"),
+            #[cfg(unix)]
+            ReadlineError::Errno(ref err) => write!(f, "Errno: {}", err.errno().desc()),
         }
     }
 }
@@ -38,10 +40,11 @@ impl error::Error for ReadlineError {
     fn description(&self) -> &str {
         match *self {
             ReadlineError::Io(ref err) => err.description(),
-            ReadlineError::Errno(ref err) => err.errno().desc(),
             ReadlineError::Char(ref err) => err.description(),
             ReadlineError::Eof => "EOF",
             ReadlineError::Interrupted => "Interrupted",
+            #[cfg(unix)]
+            ReadlineError::Errno(ref err) => err.errno().desc(),
         }
     }
 }
@@ -52,6 +55,7 @@ impl From<io::Error> for ReadlineError {
     }
 }
 
+#[cfg(unix)]
 impl From<nix::Error> for ReadlineError {
     fn from(err: nix::Error) -> ReadlineError {
         ReadlineError::Errno(err)
@@ -65,6 +69,7 @@ impl From<char_iter::CharsError> for ReadlineError {
 }
 
 impl ReadlineError {
+    #[cfg(unix)]
     pub fn from_errno(errno: nix::errno::Errno) -> ReadlineError {
         ReadlineError::from(nix::Error::from_errno(errno))
     }

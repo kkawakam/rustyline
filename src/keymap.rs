3e293b2ccbc18b66c09b160cfcff65fa71008448
gwenn <gtreguier@gmail.com>
2016-12-11 11:33:14 +0100

    Second draft for vi mode support (#94)

--- src/keymap.rs
+++ src/keymap.rs
@@ -10,13 +10,11 @@ pub enum Cmd {
     AcceptLine, // Command For History
     BackwardChar, // Command For Moving
     BackwardDeleteChar, // Command For Text
-    BackwardKillWord, // Command For Killing
-    BackwardWord, // Command For Moving
+    BackwardKillWord(Word), // Command For Killing
+    BackwardWord(Word), // Command For Moving
     BeginningOfHistory, // Command For History
     BeginningOfLine, // Command For Moving
     CapitalizeWord, // Command For Text
-    CharacterSearch(bool), // Miscellaneous Command (TODO Move right to the next occurance of c)
-    CharacterSearchBackward(bool), /* Miscellaneous Command (TODO Move left to the previous occurance of c) */
     ClearScreen, // Command For Moving
     Complete, // Command For Completion
     DeleteChar, // Command For Text
@@ -26,12 +24,12 @@ pub enum Cmd {
     EndOfLine, // Command For Moving
     ForwardChar, // Command For Moving
     ForwardSearchHistory, // Command For History
-    ForwardWord, // Command For Moving
+    ForwardWord(Word), // Command For Moving
     KillLine, // Command For Killing
     KillWholeLine, // Command For Killing (TODO Delete current line)
-    KillWord, // Command For Killing
+    KillWord(Word), // Command For Killing
     NextHistory, // Command For History
-    Noop,
+    Noop, // TODO
     PreviousHistory, // Command For History
     QuotedInsert, // Command For Text
     Replace, // TODO DeleteChar + SelfInsert
@@ -41,16 +39,37 @@ pub enum Cmd {
     TransposeWords, // Command For Text
     Unknown,
     UnixLikeDiscard, // Command For Killing
-    UnixWordRubout, // Command For Killing
+    // UnixWordRubout, // = KillWord(Word::BigWord) Command For Killing
     UpcaseWord, // Command For Text
+    ViCharSearch(CharSearch), // TODO
+    ViEndWord(Word), // TODO
+    ViKillTo(CharSearch), // TODO
     Yank, // Command For Killing
     YankPop, // Command For Killing
+}
+
+pub enum Word {
+    // non-blanks characters
+    BigWord,
+    // alphanumeric characters
+    Word,
+    // alphanumeric (and '_') characters
+    ViWord,
+}
+
+pub enum CharSearch {
+    Forward(char),
+    // until
+    ForwardBefore(char),
+    Backward(char),
+    // until
+    BackwardAfter(char),
 }
 
 // TODO numeric arguments: http://web.mit.edu/gnu/doc/html/rlman_1.html#SEC7
 pub struct EditState {
     mode: EditMode,
-    // TODO Validate Vi Command, Insert, Visual mode
+    // Vi Command/Alternate, Insert/Input mode
     insert: bool, // vi only ?
 }
 
@@ -107,20 +126,20 @@ impl EditState {
             KeyPress::Ctrl('T') => Cmd::TransposeChars,
             KeyPress::Ctrl('U') => Cmd::UnixLikeDiscard,
             KeyPress::Ctrl('V') => Cmd::QuotedInsert,
-            KeyPress::Ctrl('W') => Cmd::UnixWordRubout,
+            KeyPress::Ctrl('W') => Cmd::KillWord(Word::BigWord),
             KeyPress::Ctrl('Y') => Cmd::Yank,
-            KeyPress::Meta('\x08') => Cmd::BackwardKillWord,
-            KeyPress::Meta('\x7f') => Cmd::BackwardKillWord,
+            KeyPress::Meta('\x08') => Cmd::BackwardKillWord(Word::Word),
+            KeyPress::Meta('\x7f') => Cmd::BackwardKillWord(Word::Word),
             // KeyPress::Meta('-') => { // digit-argument
             // }
             // KeyPress::Meta('0'...'9') => { // digit-argument
             // }
             KeyPress::Meta('<') => Cmd::BeginningOfHistory,
             KeyPress::Meta('>') => Cmd::EndOfHistory,
-            KeyPress::Meta('B') => Cmd::BackwardWord,
+            KeyPress::Meta('B') => Cmd::BackwardWord(Word::Word),
             KeyPress::Meta('C') => Cmd::CapitalizeWord,
-            KeyPress::Meta('D') => Cmd::KillWord,
-            KeyPress::Meta('F') => Cmd::ForwardWord,
+            KeyPress::Meta('D') => Cmd::KillWord(Word::Word),
+            KeyPress::Meta('F') => Cmd::ForwardWord(Word::Word),
             KeyPress::Meta('L') => Cmd::DowncaseWord,
             KeyPress::Meta('T') => Cmd::TransposeWords,
             KeyPress::Meta('U') => Cmd::UpcaseWord,
@@ -137,10 +156,12 @@ impl EditState {
         let key = try!(rdr.next_key(config.keyseq_timeout()));
         let cmd = match key {
             KeyPress::Char('$') => Cmd::EndOfLine,
+            KeyPress::End => Cmd::EndOfLine,
             // TODO KeyPress::Char('%') => Cmd::???, Move to the corresponding opening/closing bracket
             KeyPress::Char('0') => Cmd::BeginningOfLine, // vi-zero: Vi move to the beginning of line.
+            KeyPress::Home => Cmd::BeginningOfLine,
             // KeyPress::Char('1'...'9') => Cmd::???, // vi-arg-digit
-            KeyPress::Char('^') => Cmd::BeginningOfLine, // TODO Move to the first non-blank character of line.
+            KeyPress::Char('^') => Cmd::BeginningOfLine, // vi-first-print TODO Move to the first non-blank character of line.
             KeyPress::Char('a') => {
                 // vi-append-mode: Vi enter insert mode after the cursor.
                 self.insert = true;
@@ -151,79 +172,41 @@ impl EditState {
                 self.insert = true;
                 Cmd::EndOfLine
             }
-            KeyPress::Char('b') => Cmd::BackwardWord,
-            // TODO KeyPress::Char('B') => Cmd::???, Move one non-blank word left.
+            KeyPress::Char('b') => Cmd::BackwardWord(Word::ViWord), // vi-prev-word
+            KeyPress::Char('B') => Cmd::BackwardWord(Word::BigWord),
             KeyPress::Char('c') => {
                 self.insert = true;
-                let mvt = try!(rdr.next_key(config.keyseq_timeout()));
-                match mvt {
-                    KeyPress::Char('$') => Cmd::KillLine, // vi-change-to-eol: Vi change to end of line.
-                    KeyPress::Char('0') => Cmd::UnixLikeDiscard,
-                    KeyPress::Char('c') => Cmd::KillWholeLine,
-                    // TODO KeyPress::Char('f') => ???,
-                    // TODO KeyPress::Char('F') => ???,
-                    KeyPress::Char('h') => Cmd::BackwardDeleteChar,
-                    KeyPress::Char('l') => Cmd::DeleteChar,
-                    KeyPress::Char(' ') => Cmd::DeleteChar,
-                    // TODO KeyPress::Char('t') => ???,
-                    // TODO KeyPress::Char('T') => ???,
-                    KeyPress::Char('w') => Cmd::KillWord,
-                    _ => Cmd::Unknown,
-                }
+                try!(self.vi_delete_motion(rdr, config, key))
             }
             KeyPress::Char('C') => {
                 self.insert = true;
                 Cmd::KillLine
             }
-            KeyPress::Char('d') => {
-                let mvt = try!(rdr.next_key(config.keyseq_timeout()));
-                match mvt {
-                    KeyPress::Char('$') => Cmd::KillLine,
-                    KeyPress::Char('0') => Cmd::UnixLikeDiscard, // vi-kill-line-prev: Vi cut from beginning of line to cursor.
-                    KeyPress::Char('d') => Cmd::KillWholeLine,
-                    // TODO KeyPress::Char('f') => ???,
-                    // TODO KeyPress::Char('F') => ???,
-                    KeyPress::Char('h') => Cmd::BackwardDeleteChar, // vi-delete-prev-char: Vi move to previous character (backspace).
-                    KeyPress::Char('l') => Cmd::DeleteChar,
-                    KeyPress::Char(' ') => Cmd::DeleteChar,
-                    // TODO KeyPress::Char('t') => ???,
-                    // TODO KeyPress::Char('T') => ???,
-                    KeyPress::Char('w') => Cmd::KillWord,
-                    _ => Cmd::Unknown,
-                }
-            }
+            KeyPress::Char('d') => try!(self.vi_delete_motion(rdr, config, key)),
             KeyPress::Char('D') => Cmd::KillLine,
-            // TODO KeyPress::Char('e') => Cmd::???, vi-to-end-word: Vi move to the end of the current word. Move to the end of the current word.
-            // TODO KeyPress::Char('E') => Cmd::???, vi-end-word: Vi move to the end of the current space delimited word. Move to the end of the current non-blank word.
+            KeyPress::Char('e') => Cmd::ViEndWord(Word::ViWord),
+            KeyPress::Char('E') => Cmd::ViEndWord(Word::BigWord),
             KeyPress::Char('i') => {
-                // vi-insert: Vi enter insert mode.
+                // vi-insertion-mode
                 self.insert = true;
                 Cmd::Noop
             }
             KeyPress::Char('I') => {
-                // vi-insert-at-bol: Vi enter insert mode at the beginning of line.
+                // vi-insert-beg
                 self.insert = true;
                 Cmd::BeginningOfLine
             }
-            KeyPress::Char('f') => {
-                // vi-next-char: Vi move to the character specified next.
-                let ch = try!(rdr.next_key(config.keyseq_timeout()));
-                match ch {
-                    KeyPress::Char(_) => return Ok((ch, Cmd::CharacterSearch(false))),
-                    _ => Cmd::Unknown,
-                }
-            }
-            KeyPress::Char('F') => {
-                // vi-prev-char: Vi move to the character specified previous.
-                let ch = try!(rdr.next_key(config.keyseq_timeout()));
-                match ch {
-                    KeyPress::Char(_) => return Ok((ch, Cmd::CharacterSearchBackward(false))),
-                    _ => Cmd::Unknown,
+            KeyPress::Char(c) if c == 'f' || c == 'F' || c == 't' || c == 'T' => {
+                // vi-char-search
+                let cs = try!(self.vi_char_search(rdr, config, c));
+                match cs {
+                    Some(cs) => Cmd::ViCharSearch(cs),
+                    None => Cmd::Unknown,
                 }
             }
             // TODO KeyPress::Char('G') => Cmd::???, Move to the history line n
-            KeyPress::Char('p') => Cmd::Yank, // vi-paste-next: Vi paste previous deletion to the right of the cursor.
-            KeyPress::Char('P') => Cmd::Yank, // vi-paste-prev: Vi paste previous deletion to the left of the cursor. TODO Insert the yanked text before the cursor.
+            KeyPress::Char('p') => Cmd::Yank, // vi-put
+            KeyPress::Char('P') => Cmd::Yank, // vi-put TODO Insert the yanked text before the cursor.
             KeyPress::Char('r') => {
                 // vi-replace-char: Vi replace character under the cursor with the next character typed.
                 let ch = try!(rdr.next_key(config.keyseq_timeout()));
@@ -244,35 +227,20 @@ impl EditState {
                 self.insert = true;
                 Cmd::KillWholeLine
             }
-            KeyPress::Char('t') => {
-                // vi-to-next-char: Vi move up to the character specified next.
-                let ch = try!(rdr.next_key(config.keyseq_timeout()));
-                match ch {
-                    KeyPress::Char(_) => return Ok((ch, Cmd::CharacterSearchBackward(true))),
-                    _ => Cmd::Unknown,
-                }
-            }
-            KeyPress::Char('T') => {
-                // vi-to-prev-char: Vi move up to the character specified previous.
-                let ch = try!(rdr.next_key(config.keyseq_timeout()));
-                match ch {
-                    KeyPress::Char(_) => return Ok((ch, Cmd::CharacterSearch(true))),
-                    _ => Cmd::Unknown,
-                }
-            }
             // KeyPress::Char('U') => Cmd::???, // revert-line
-            KeyPress::Char('w') => Cmd::ForwardWord, // vi-next-word: Vi move to the next word.
-            // TODO KeyPress::Char('W') => Cmd::???, // vi-next-space-word: Vi move to the next space delimited word. Move one non-blank word right.
+            KeyPress::Char('w') => Cmd::ForwardWord(Word::ViWord), // vi-next-word
+            KeyPress::Char('W') => Cmd::ForwardWord(Word::BigWord), // vi-next-word
             KeyPress::Char('x') => Cmd::DeleteChar, // vi-delete: TODO move backward if eol
-            KeyPress::Char('X') => Cmd::BackwardDeleteChar,
-            KeyPress::Home => Cmd::BeginningOfLine,
+            KeyPress::Char('X') => Cmd::BackwardDeleteChar, // vi-rubout
+            // KeyPress::Char('y') => Cmd::???, // vi-yank-to
+            // KeyPress::Char('Y') => Cmd::???, // vi-yank-to
             KeyPress::Char('h') => Cmd::BackwardChar,
+            KeyPress::Ctrl('H') => Cmd::BackwardChar,
+            KeyPress::Backspace => Cmd::BackwardChar, // TODO Validate
             KeyPress::Left => Cmd::BackwardChar,
             KeyPress::Ctrl('D') => Cmd::EndOfFile,
             KeyPress::Delete => Cmd::DeleteChar,
-            KeyPress::End => Cmd::EndOfLine,
             KeyPress::Ctrl('G') => Cmd::Abort,
-            KeyPress::Ctrl('H') => Cmd::BackwardChar,
             KeyPress::Char('l') => Cmd::ForwardChar,
             KeyPress::Char(' ') => Cmd::ForwardChar,
             KeyPress::Right => Cmd::ForwardChar,
@@ -294,7 +262,7 @@ impl EditState {
             KeyPress::Ctrl('T') => Cmd::TransposeChars,
             KeyPress::Ctrl('U') => Cmd::UnixLikeDiscard,
             KeyPress::Ctrl('V') => Cmd::QuotedInsert,
-            KeyPress::Ctrl('W') => Cmd::UnixWordRubout,
+            KeyPress::Ctrl('W') => Cmd::KillWord(Word::BigWord),
             KeyPress::Ctrl('Y') => Cmd::Yank,
             KeyPress::Esc => Cmd::Noop,
             _ => Cmd::Unknown,
@@ -324,7 +292,7 @@ impl EditState {
             KeyPress::Ctrl('T') => Cmd::TransposeChars,
             KeyPress::Ctrl('U') => Cmd::UnixLikeDiscard,
             KeyPress::Ctrl('V') => Cmd::QuotedInsert,
-            KeyPress::Ctrl('W') => Cmd::UnixWordRubout,
+            KeyPress::Ctrl('W') => Cmd::KillWord(Word::BigWord),
             KeyPress::Ctrl('Y') => Cmd::Yank,
             KeyPress::Esc => {
                 // vi-movement-mode/vi-command-mode: Vi enter command mode (use alternative key bindings).
@@ -334,5 +302,57 @@ impl EditState {
             _ => Cmd::Unknown,
         };
         Ok((key, cmd))
+    }
+
+    fn vi_delete_motion<R: RawReader>(&mut self,
+                                      rdr: &mut R,
+                                      config: &Config,
+                                      key: KeyPress)
+                                      -> Result<Cmd> {
+        let mvt = try!(rdr.next_key(config.keyseq_timeout()));
+        Ok(match mvt {
+            KeyPress::Char('$') => Cmd::KillLine, // vi-change-to-eol: Vi change to end of line.
+            KeyPress::Char('0') => Cmd::UnixLikeDiscard, // vi-kill-line-prev: Vi cut from beginning of line to cursor.
+            KeyPress::Char('b') => Cmd::BackwardKillWord(Word::ViWord),
+            KeyPress::Char('B') => Cmd::BackwardKillWord(Word::BigWord),
+            x if x == key => Cmd::KillWholeLine,
+            KeyPress::Char('e') => Cmd::KillWord(Word::ViWord),
+            KeyPress::Char('E') => Cmd::KillWord(Word::BigWord),
+            KeyPress::Char(c) if c == 'f' || c == 'F' || c == 't' || c == 'T' => {
+                let cs = try!(self.vi_char_search(rdr, config, c));
+                match cs {
+                    Some(cs) => Cmd::ViKillTo(cs),
+                    None => Cmd::Unknown,
+                }
+            }
+            KeyPress::Char('h') => Cmd::BackwardDeleteChar, // vi-delete-prev-char: Vi move to previous character (backspace).
+            KeyPress::Ctrl('H') => Cmd::BackwardDeleteChar,
+            KeyPress::Backspace => Cmd::BackwardDeleteChar,
+            KeyPress::Char('l') => Cmd::DeleteChar,
+            KeyPress::Char(' ') => Cmd::DeleteChar,
+            KeyPress::Char('w') => Cmd::KillWord(Word::ViWord),
+            KeyPress::Char('W') => Cmd::KillWord(Word::BigWord),
+            _ => Cmd::Unknown,
+        })
+    }
+
+    fn vi_char_search<R: RawReader>(&mut self,
+                                    rdr: &mut R,
+                                    config: &Config,
+                                    cmd: char)
+                                    -> Result<Option<CharSearch>> {
+        let ch = try!(rdr.next_key(config.keyseq_timeout()));
+        Ok(match ch {
+            KeyPress::Char(ch) => {
+                Some(match cmd {
+                    'f' => CharSearch::Forward(ch),
+                    't' => CharSearch::ForwardBefore(ch),
+                    'F' => CharSearch::Backward(ch),
+                    'T' => CharSearch::BackwardAfter(ch),
+                    _ => unreachable!(),
+                })
+            }
+            _ => None,
+        })
     }
 }

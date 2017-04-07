c2000f2756e27df49f95171f845a80b0b41e6f2e
gwenn <gtreguier@gmail.com>
2017-04-07 19:03:14 +0200

    Rustfmt
    Some blocks are not correctly aligned/shifted (see keymap.rs).

--- src/lib.rs
+++ src/lib.rs
@@ -217,8 +217,9 @@ impl<'out, 'prompt> State<'out, 'prompt> {
                  .set_console_cursor_position(info.dwCursorPosition));
         let mut _count = 0;
         try!(self.term
-            .fill_console_output_character((info.dwSize.X * (self.old_rows as i16 + 1)) as u32,
-                                           info.dwCursorPosition));
+                 .fill_console_output_character((info.dwSize.X * (self.old_rows as i16 + 1)) as
+                                                u32,
+                                                info.dwCursorPosition));
         let mut ab = String::new();
         // display the prompt
         ab.push_str(prompt); // TODO handle ansi escape code (SetConsoleTextAttribute)

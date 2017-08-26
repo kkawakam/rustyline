42c24367023ee3ad9a2042a9f28f46f24ee0c5c0
Katsu Kawakami <kkawa1570@gmail.com>
2017-08-26 23:07:09 +0000

    Really fix the documentation deploys...

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -11,11 +11,11 @@ git config user.name "Katsu Kawakami"
 git config user.email "kkawa1570@gmail.com"
 
 git remote add upstream "https://$GH_TOKEN@github.com/kkawakam/rustyline.git"
-git fetch upstream 
-git push origin --delete gh_pages > /dev/null 2>&1
+git fetch upstream
+git push upstream --delete gh-pages > /dev/null 2>&1
 
 touch .
 
 git add -A .
 git commit -m "rebuild pages at ${rev}"
-git push -f -q upstream gh-pages > /dev/null 2>&1
+git push -q upstream HEAD:gh-pages > /dev/null 2>&1

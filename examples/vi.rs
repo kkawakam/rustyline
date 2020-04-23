23e05df642676d74e31d9ddc3de4c3a6582072c2
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2020-04-22 20:34:38 -0400

    Simplify Vi example

--- examples/vi.rs
+++ examples/vi.rs
@@ -1,42 +1,20 @@
+//! This example shows how a highlighter can be used to show Vi mode indicators
+//! while editing.
+
 use std::borrow::Cow::{self, Borrowed, Owned};
 
-use rustyline::completion::{Completer, FilenameCompleter, Pair};
-use rustyline::config::OutputStreamType;
-use rustyline::error::ReadlineError;
-use rustyline::highlight::{Highlighter, MatchingBracketHighlighter, PromptState};
-use rustyline::hint::{Hinter, HistoryHinter};
-use rustyline::validate::{self, MatchingBracketValidator, Validator};
-use rustyline::{Cmd, CompletionType, Config, Context, EditMode, Editor, InputMode, KeyPress};
-use rustyline_derive::Helper;
+use rustyline::highlight::{Highlighter, PromptState};
+use rustyline::{Config, EditMode, Editor, InputMode};
+use rustyline_derive::{Completer, Helper, Hinter, Validator};
 
-#[derive(Helper)]
-struct MyHelper {
-    completer: FilenameCompleter,
-    highlighter: MatchingBracketHighlighter,
-    validator: MatchingBracketValidator,
-    hinter: HistoryHinter,
+#[derive(Completer, Helper, Hinter, Validator)]
+struct ViHighlighter {
+    command: &'static str,
+    insert: &'static str,
+    replace: &'static str,
 }
 
-impl Completer for MyHelper {
-    type Candidate = Pair;
-
-    fn complete(
-        &self,
-        line: &str,
-        pos: usize,
-        ctx: &Context<'_>,
-    ) -> Result<(usize, Vec<Pair>), ReadlineError> {
-        self.completer.complete(line, pos, ctx)
-    }
-}
-
-impl Hinter for MyHelper {
-    fn hint(&self, line: &str, pos: usize, ctx: &Context<'_>) -> Option<String> {
-        self.hinter.hint(line, pos, ctx)
-    }
-}
-
-impl Highlighter for MyHelper {
+impl Highlighter for ViHighlighter {
     fn highlight_prompt<'b, 's: 'b, 'p: 'b>(
         &'s self,
         prompt: &'p str,
@@ -44,89 +22,30 @@ impl Highlighter for MyHelper {
     ) -> Cow<'b, str> {
         if prompt_state.default {
             let indicator = match prompt_state.mode {
-                Some(InputMode::Command) => ":",
-                Some(InputMode::Insert) => "+",
-                Some(InputMode::Replace) => "#",
+                Some(InputMode::Command) => self.command,
+                Some(InputMode::Insert) => self.insert,
+                Some(InputMode::Replace) => self.replace,
                 _ => " ",
             };
-            Owned(format!("\x1b[1;32m{}{}\x1b[0m", indicator, &prompt[1..]))
+            Owned(format!("{}{}", indicator, &prompt[1..]))
         } else {
             Borrowed(prompt)
         }
     }
-
-    fn highlight_hint<'h>(&self, hint: &'h str) -> Cow<'h, str> {
-        Owned("\x1b[1m".to_owned() + hint + "\x1b[m")
-    }
-
-    fn highlight<'l>(&self, line: &'l str, pos: usize) -> Cow<'l, str> {
-        self.highlighter.highlight(line, pos)
-    }
-
-    fn highlight_char(&self, line: &str, pos: usize) -> bool {
-        self.highlighter.highlight_char(line, pos)
-    }
 }
 
-impl Validator for MyHelper {
-    fn validate(
-        &self,
-        ctx: &mut validate::ValidationContext,
-    ) -> rustyline::Result<validate::ValidationResult> {
-        self.validator.validate(ctx)
-    }
-
-    fn validate_while_typing(&self) -> bool {
-        self.validator.validate_while_typing()
-    }
-}
-
-// To debug rustyline:
-// RUST_LOG=rustyline=debug cargo run --example example 2> debug.log
 fn main() -> rustyline::Result<()> {
-    env_logger::init();
-    let config = Config::builder()
-        .history_ignore_space(true)
-        .completion_type(CompletionType::List)
-        .edit_mode(EditMode::Vi)
-        .output_stream(OutputStreamType::Stdout)
-        .build();
-    let h = MyHelper {
-        completer: FilenameCompleter::new(),
-        highlighter: MatchingBracketHighlighter::new(),
-        hinter: HistoryHinter {},
-        validator: MatchingBracketValidator::new(),
+    let config = Config::builder().edit_mode(EditMode::Vi).build();
+    let helper = ViHighlighter {
+        command: ":",
+        insert: "+",
+        replace: "#",
     };
     let mut rl = Editor::with_config(config);
-    rl.set_helper(Some(h));
-    rl.bind_sequence(KeyPress::Meta('N'), Cmd::HistorySearchForward);
-    rl.bind_sequence(KeyPress::Meta('P'), Cmd::HistorySearchBackward);
-    if rl.load_history("history.txt").is_err() {
-        println!("No previous history.");
-    }
-    let mut count = 1;
-    loop {
-        let p = format!(" {}> ", count);
-        let readline = rl.readline(&p);
-        match readline {
-            Ok(line) => {
-                rl.add_history_entry(line.as_str());
-                println!("Line: {}", line);
-            }
-            Err(ReadlineError::Interrupted) => {
-                println!("CTRL-C");
-                break;
-            }
-            Err(ReadlineError::Eof) => {
-                println!("CTRL-D");
-                break;
-            }
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break;
-            }
-        }
-        count += 1;
-    }
-    rl.save_history("history.txt")
+    rl.set_helper(Some(helper));
+
+    let username = rl.readline(" > ")?;
+    println!("Echo: {}", username);
+
+    Ok(())
 }

b7789a0891c159cc5096b283712f645b9cd55079
gwenn <gtreguier@gmail.com>
2022-03-02 15:21:45 +0100

    Simplify external_print example

--- examples/external_print.rs
+++ examples/external_print.rs
@@ -3,13 +3,11 @@ use std::time::Duration;
 
 use rand::{thread_rng, Rng};
 
-use rustyline::error::ReadlineError;
-use rustyline::{Editor, ExternalPrinter};
+use rustyline::{Editor, ExternalPrinter, Result};
 
-fn main() {
-    env_logger::init();
+fn main() -> Result<()> {
     let mut rl = Editor::<()>::new();
-    let mut printer = rl.create_external_printer().expect("No printer");
+    let mut printer = rl.create_external_printer()?;
     thread::spawn(move || {
         let mut rng = thread_rng();
         let mut i = 0usize;
@@ -17,31 +15,15 @@ fn main() {
             printer
                 .print(format!("External message #{}", i))
                 .expect("External print failure");
-            let wait_ms = rng.gen_range(1..500);
+            let wait_ms = rng.gen_range(1000..10000);
             thread::sleep(Duration::from_millis(wait_ms));
             i += 1;
         }
     });
 
     loop {
-        let readline = rl.readline(">>");
-        match readline {
-            Ok(line) => {
-                rl.add_history_entry(line.as_str());
-                println!("Line: {}", line);
-            }
-            Err(ReadlineError::Interrupted) => {
-                println!("CTRL-C");
-                break;
-            }
-            Err(ReadlineError::Eof) => {
-                println!("CTRL-D");
-                break;
-            }
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break;
-            }
-        }
+        let line = rl.readline(">>")?;
+        rl.add_history_entry(line.as_str());
+        println!("Line: {}", line);
     }
 }

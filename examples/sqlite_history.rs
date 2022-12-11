736ad9c24f001bc981356fa8b70588ec5fd5ff54
gwenn <gtreguier@gmail.com>
2022-12-11 09:25:58 +0100

    Clean sqlite_history example

--- examples/sqlite_history.rs
+++ examples/sqlite_history.rs
@@ -2,7 +2,6 @@ use rustyline::{Config, Editor, Result};
 
 fn main() -> Result<()> {
     let config = Config::builder().auto_add_history(true).build();
-    #[cfg(feature = "with-sqlite-history")]
     let history = if false {
         // memory
         rustyline::sqlite_history::SQLiteHistory::with_config(config)?
@@ -10,8 +9,6 @@ fn main() -> Result<()> {
         // file
         rustyline::sqlite_history::SQLiteHistory::open(config, "history.sqlite3")?
     };
-    #[cfg(not(feature = "with-sqlite-history"))]
-    let history = rustyline::history::MemHistory::with_config(config);
     let mut rl: Editor<(), _> = Editor::with_history(config, history)?;
     loop {
         let line = rl.readline("> ")?;

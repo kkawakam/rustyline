0dde217d4a1d7f720eff121c9614bbf8349ec92b
gwenn <gtreguier@gmail.com>
2023-01-18 20:52:43 +0100

    Revert "Fix clippy warnings"
    This reverts commit 9ac04500e4cd44472633f9e04b76cde3a59c5325.

--- src/line_buffer.rs
+++ src/line_buffer.rs
@@ -26,11 +26,16 @@ pub enum WordAction {
 }
 
 /// Delete (kill) direction
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub(crate) enum Direction {
-    #[default]
     Forward,
     Backward,
+}
+
+impl Default for Direction {
+    fn default() -> Self {
+        Direction::Forward
+    }
 }
 
 /// Listener to be notified when some text is deleted.

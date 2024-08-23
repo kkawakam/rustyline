53648543f7511fb5271afed3748f2b08cc9810f3
xeruf <27jf@pm.me>
2024-08-23 11:15:42 +0300

    Implement ExternalPrinterWriter
    To enable duplex output of log messages
    
    Fixes #797

--- examples/external_print.rs
+++ examples/external_print.rs
@@ -1,5 +1,11 @@
+extern crate env_logger;
+extern crate log;
+
+use std::io::Write;
 use std::thread;
 use std::time::Duration;
+use env_logger::Target;
+use log::LevelFilter;
 
 use rand::{thread_rng, Rng};
 
@@ -8,10 +14,24 @@ use rustyline::{DefaultEditor, ExternalPrinter, Result};
 fn main() -> Result<()> {
     let mut rl = DefaultEditor::new()?;
     let mut printer = rl.create_external_printer()?;
+
+    env_logger::builder()
+        .filter_level(LevelFilter::Info)
+        .target(Target::Pipe(rl.create_external_writer()?))
+        .init();
+    thread::spawn(move || {
+        loop {
+            log::info!("Log Message");
+            thread::sleep(Duration::from_secs(3));
+        }
+    });
+
+    let mut writer = rl.create_external_writer().unwrap();
     thread::spawn(move || {
         let mut rng = thread_rng();
         let mut i = 0usize;
         loop {
+            writer.write("writing without newline".as_bytes());
             printer
                 .print(format!("External message #{i}"))
                 .expect("External print failure");
@@ -23,7 +43,6 @@ fn main() -> Result<()> {
 
     loop {
         let line = rl.readline("> ")?;
-        rl.add_history_entry(line.as_str())?;
-        println!("Line: {line}");
+        println!("Read Line: {line}");
     }
 }
